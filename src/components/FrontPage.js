8c136a6e037c4bed069e8e1d8f22190669135707
Jose L. Velez <joselvelez@protonmail.com>
2021-11-11 22:39:01 -0600

    css work

--- src/components/FrontPage.js
+++ src/components/FrontPage.js
@@ -1,8 +1,8 @@
 export const FrontPage = () => {
     return (
         <div className="flex bg-gray-100 justify-center">
-            <div className="flex flex-row w-2/3 justify-between pl-0 pr-10 py-10">
-                <div className="w-2/3 pl-5 pr-14">
+            <div className="flex flex-row sm:w-full lg:w-2/3 justify-between pl-0 pr-10 py-10">
+                <div className="sm:w-full lg:w-2/3 pl-5 pr-14">
                     <p className="pb-2 leading-relaxed font-sans text-sm">Hello, I'm <b className="text-gray-700">Jose Velez</b></p>
                     <p className="py-2 leading-relaxed font-sans text-sm">
                         I spent 10 years working in the energy industry as a compliance professional and another 5 or so years working in finance. During that time, I was always thinking about technology and concepts for building web applications. I had avoided learning to write code because it intimidated me and I was unsure of myself, thus shelving many ideas I had come up with.
@@ -23,9 +23,9 @@ export const FrontPage = () => {
                             <a href="https://ethereum.org" className="px-1 text-yellow-700 hover:text-yellow-500 text-sm" target="_blank" rel="noreferrer">ethereum</a>
                     </p>
                 </div>
-                <div className="flex sm:content-start md:content-start w-1/3">
+                <div className="flex sm:content-start hidden lg:block lg:content-start w-1/3">
                     <div>
-                        <img src="/images/48886275.jpg" className="rounded-full sm:hidden md:block" alt="profile" />
+                        <img src="/images/48886275.jpg" className="rounded-full" alt="profile" />
                     </div>
                 </div>
             </div>
